68f7f3612d5392d07859ed95a3662b730b688aa6
Gautham Nair <gautham.p.nair@gmail.com>
2014-08-04 20:19:38 -0400

    moved importing of weekly commits into models and extracted method

--- gitModels.py
+++ gitModels.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy as sa
+import datetime
 
 import logging
 
@@ -49,6 +50,7 @@ class Repo(Base):
 	}
 	_doNotGetFromPyGithubObject = ['id']
 
+
 class WeeklyContribution(Base):
 	__tablename__ = 'WeeklyContributions'
 	id = Column(sa.Integer, primary_key=True)
@@ -58,7 +60,6 @@ class WeeklyContribution(Base):
 	commits_num = Column(sa.Integer)
 	additions_num = Column(sa.Integer)
 	deletions_num = Column(sa.Integer)
-
 
 
 Base.metadata.create_all(engine)
@@ -89,5 +90,25 @@ def makeRepo(pyGithubRepo):
 	theRepo = Repo(**kvpairs)
 	session.add(theRepo)
 	return theRepo
+
+
+def storeWeeklyContributions(pyGithubRepo):
+	weeklyContributions = pyGithubRepo.get_stats_contributors()
+
+	for contributor in weeklyContributions:
+		author_login = contributor.author.login
+		repo_full_name = pyGithubRepo.full_name
+		weeks = [week for week in contributor.raw_data['weeks'] if week['c'] > 0]
+
+		for week in weeks:
+			wm = WeeklyContribution(
+				author_login=author_login,
+				repo_full_name=repo_full_name,
+				week_start=datetime.datetime.fromtimestamp(week['w']),
+				commits_num = week['c'],
+				additions_num = week['a'],
+				deletions_num = week['d']
+			)
+			session.add(wm)
 
 
--- test_sqlalchemy.py
+++ test_sqlalchemy.py
@@ -15,23 +15,5 @@ plyr = importer.getRepo('hadley/plyr')
 # assert(fromdb.full_name == stored.full_name)
 # gitModels.session.commit()
 
-
-weeklyContributions = plyr.get_stats_contributors()
-
-contributor = weeklyContributions[-1]
-author_login = contributor.author.login
-repo_full_name = plyr.full_name
-weeks = [week for week in contributor.raw_data['weeks'] if week['c'] > 0]
-
-for week in weeks:
-	wm = gitModels.WeeklyContribution(
-		author_login=author_login,
-		repo_full_name=repo_full_name,
-		week_start=datetime.datetime.fromtimestamp(week['w']),
-		commits_num = week['c'],
-		additions_num = week['a'],
-		deletions_num = week['d']
-	)
-	gitModels.session.add(wm)
-
+gitModels.storeWeeklyContributions(plyr)
 gitModels.session.commit()
