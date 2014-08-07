41a1b0c67110f872f19fb288c9dd8966d0f85807
Gautham Nair <gautham.p.nair@gmail.com>
2014-08-07 10:35:09 -0400

    a working post request.

--- app/views.py
+++ app/views.py
@@ -1,13 +1,17 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
 import pymysql as mdb
+import datetime
 
 db = mdb.connect(user="root", host="localhost", db="world_innodb", charset='utf8')
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = { 'nickname': 'Miguelito'}
-	return render_template("index.html",
-		title='Home',
-		user = user)
+	return render_template("index.html", theTime=datetime.datetime.today())
+
+@app.route('/mockpost', methods=['POST'])
+def mockpost():
+	print "\nThe Field is"
+	print request.form["somefield"], "\n\n"
+	return redirect("/index")
