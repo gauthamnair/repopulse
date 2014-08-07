8c051c4ccb5ad9d25c5669f9556d385116fe8c8c
Gautham Nair <gautham.p.nair@gmail.com>
2014-08-07 17:29:51 -0400

    refactored training data extractor to use featuremaker class

--- extractFeaturesToFile.py
+++ extractFeaturesToFile.py
@@ -1,29 +1,25 @@
-from featureMakers import makeFeatureMakers, defaultTref, howToAggregateAuthors
+import featureMakers
 import pandas as pd
 import pymysql as mdb
 con = mdb.connect('localhost', 'root', '', 'gitdb');
 
 query = "SELECT * FROM WeeklyContributions"
-contrib = pd.io.sql.read_sql(query, con)
-contrib.index = contrib['week_start']
+weeklyData = pd.io.sql.read_sql(query, con)
+weeklyData.index = weeklyData['week_start']
 
-tref = defaultTref()
-featureMakers = makeFeatureMakers(tref=tref)
-grouped = contrib.groupby(['repo_full_name', 'author_login'], as_index=False)
-commitsFeatures = grouped['commits_num'].aggregate(featureMakers)['commits_num']
+tref = featureMakers.defaultTref()
+fmaker = featureMakers.FeatureMaker(tref=tref)
 
+commitStatsByRepoDicts = []
+for repo_full_name, repoWeeklyData in weeklyData.groupby('repo_full_name'):
+	features = fmaker.makeAuthorAggregatedFeatures(repoWeeklyData)
+	features['repo_full_name'] = repo_full_name
+	commitStatsByRepoDicts.append(features)
 
-commitStatsByRepo = commitsFeatures.groupby(level=0).aggregate(howToAggregateAuthors)
-commitStatsByRepo = commitStatsByRepo[~ commitStatsByRepo['daysSinceLastCommit'].isnull()]
+commitStatsByRepo = pd.DataFrame(commitStatsByRepoDicts)
 
-query = "SELECT full_name, created_at, downloaded_on, language FROM Repos"
-repos = pd.io.sql.read_sql(query, con)
+def removeReposCreatedAfterTref(df):
+	return df[~ df['daysSinceLastCommit'].isnull()]
 
-timeSinceCreation = [tref - t.to_datetime().date() for t in repos['created_at']]
-repos['daysSinceCreation'] = [t.days for t in timeSinceCreation]
-repoStats = repos[['full_name', 'daysSinceCreation', 'language']]
-
-byRepo = pd.merge(repoStats, commitStatsByRepo,
-	how='inner', left_on='full_name', right_index=True)
-
-byRepo.to_csv('data/intermediate/byRepo.csv')
+commitStatsByRepo = removeReposCreatedAfterTref(commitStatsByRepo)
+commitStatsByRepo.to_csv('data/intermediate/byRepo.csv')
