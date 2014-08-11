a824676284c55f7db80ea6254dd6762847fe33e6
Gautham Nair <gautham.p.nair@gmail.com>
2014-08-11 17:45:40 -0400

    modularized extract features to file

--- extractFeaturesToFile.py
+++ extractFeaturesToFile.py
@@ -1,25 +1,40 @@
-import featureMakers
 import pandas as pd
 import pymysql as mdb
 con = mdb.connect('localhost', 'root', '', 'gitdb');
 
-query = "SELECT * FROM WeeklyContributions"
-weeklyData = pd.io.sql.read_sql(query, con)
-featureMakers.removeTimeFromWeekStartDate(weeklyData)
+_extractedFileName = 'data/intermediate/byRepo.csv'
 
-tref = featureMakers.defaultTref()
-fmaker = featureMakers.FeatureMaker(tref=tref)
+def computeAndSaveToFile():
 
-commitStatsByRepoDicts = []
-for repo_full_name, repoWeeklyData in weeklyData.groupby('repo_full_name'):
-	features = fmaker.makeFeatures(repoWeeklyData)
-	features['repo_full_name'] = repo_full_name
-	commitStatsByRepoDicts.append(features)
+	import featureMakers
 
-commitStatsByRepo = pd.DataFrame(commitStatsByRepoDicts)
+	query = "SELECT * FROM WeeklyContributions"
+	weeklyData = pd.io.sql.read_sql(query, con)
+	featureMakers.removeTimeFromWeekStartDate(weeklyData)
 
-def removeReposCreatedAfterTref(df):
-	return df[~ df['daysSinceLastCommit'].isnull()]
+	tref = featureMakers.defaultTref()
+	fmaker = featureMakers.FeatureMaker(tref=tref)
 
-commitStatsByRepo = removeReposCreatedAfterTref(commitStatsByRepo)
-commitStatsByRepo.to_csv('data/intermediate/byRepo.csv')
+	commitStatsByRepoDicts = []
+	for repo_full_name, repoWeeklyData in weeklyData.groupby('repo_full_name'):
+		features = fmaker.makeFeatures(repoWeeklyData)
+		features['repo_full_name'] = repo_full_name
+		commitStatsByRepoDicts.append(features)
+
+	commitStatsByRepo = pd.DataFrame(commitStatsByRepoDicts)
+
+	def removeReposCreatedAfterTref(df):
+		return df[~ df['daysSinceLastCommit'].isnull()]
+
+	commitStatsByRepo = removeReposCreatedAfterTref(commitStatsByRepo)
+	commitStatsByRepo.to_csv(_extractedFileName)
+
+
+def loadFromFile():
+	byRepo = pd.read_csv(_extractedFileName)
+	del byRepo['Unnamed: 0']
+	return byRepo
+
+
+if __name__ == '__main__':
+	computeAndSaveToFile()
