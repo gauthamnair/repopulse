7c3efb31c8b62546c96eaa2ae25cf03f3af15529
Gautham Nair <gautham.p.nair@gmail.com>
2014-08-14 12:41:57 -0400

    a graph of gap length histograms compared to distribution of the days since last commit

--- analyzeGaps.R
+++ analyzeGaps.R
@@ -1,9 +1,41 @@
 library(ggplot2)
 library(plyr)
 library(reshape2)
+library(grid)
+library(data.table)
 
-gaps <- read.csv('data/intermediate/gaps.csv')
+gaps <- read.csv('data/intermediate/gapsToday.csv')
+features <- read.csv('data/intermediate/byRepoToday.csv')
 
-q <- ggplot(gaps, aes(x=log10(gap_length_days) )) + geom_bar()
-q <- q + geom_vline(x=log10(c(365/2, 365)))
-print(q)
+gaps <- data.table(gaps)
+
+longestgap <- gaps[, list(longest_gap_days = max(gap_length_days)),
+	by='repo_full_name']
+
+graphFileName <- function(str){
+	return(paste0('data/graphs/', str, '.pdf'))
+}
+
+
+binwidth <- 0.05
+q_gaps <- ggplot(gaps, aes(x=log10(gap_length_days + 1) )) + 
+	geom_bar(binwidth=binwidth)
+q_gaps <- q_gaps + geom_vline(x=log10(c(365/2, 365)))
+# print(q_gaps)
+
+q_longest <- ggplot(longestgap, aes(x=log10(longest_gap_days + 1) )) + 
+	geom_bar(binwidth=binwidth)
+q_longest <- q_longest + geom_vline(x=log10(c(365/2, 365)))
+# print(q_longest)
+
+q_lastcommit <- ggplot(features, aes(x=log10(daysSinceLastCommit + 1))) + 
+	geom_bar(binwidth=binwidth)
+q_lastcommit <- q_lastcommit + geom_vline(x=log10(c(365/2, 365)))
+# print(q_lastcommit)
+
+pdf(graphFileName('gapStats'), width=5, height=6)
+pushViewport(viewport(layout=grid.layout(3,1)))
+print(q_gaps + xlim(0,3.5), vp = viewport(layout.pos.row=1))
+print(q_longest + xlim(0,3.5), vp = viewport(layout.pos.row=2))
+print(q_lastcommit + xlim(0,3.5), vp = viewport(layout.pos.row=3))
+dev.off()
